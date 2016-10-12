9f4a969b4193d93cde32550f207612bfb47274c4
Adam Greenough <adam@amazorize.com>
2016-10-12 04:14:00 +0100

    Adding about page template

---
+++ page-about.php
@@ -0,0 +1,37 @@
+<?php theme_include('header'); ?>
+
+		<section class="content default-page convert-emoji">
+			<div class="row">
+				<div class="columns large-10 large-offset-1">
+					<h1><?php echo page_title(); ?></h1>
+	
+					<p>Hello! My name's Adam, 21 years young from the sunny south coast of the United Kingdom. By day, I am a design-orientated developer and digital consultant. By night, well, mostly that too.</p>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="columns large-6">
+					<img src="/themes/adam/assets/about/cruise-pic.jpg">
+					<div class="caption">Sasha and I aboard the <span class="small-caps">MS Queen Elizabeth</span> en route to Hamburg, August 2016.</div>
+				</div>
+			
+				<div class="columns large-5">		
+					<p>On the rare occasion I get some free time I enjoy being on the water, anything that will float; playing the piano, attempting to at least; and spending time with my girlfriend, Sasha.</p>
+					
+					<p>As a digital native, I have always been fascinated by the limitless possibilities of the internet. I aspire to use my passion for the medium to be a part of something that changes the world for the better, one day. :v::peace:</p>
+				</div>
+				
+				<div class="columns large-1">
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="columns large-10 large-offset-1">
+					<p>For now though, this website serves as a personal playground and home of the occasional blog post. I have never been much of a writer, as much as I love the idea, so I like to think of my posts as little works of digital art and a way of trying out new techniques that are a little too experimental for my day-to-day client projects. You should <a href="/posts/">check them out</a>.</p>
+					
+					<p>If for one reason or another you'd like to contact me, <a href="https://twitter.com/adam_greenough" class="twitter" target="_blank">Twitter</a> is normally a good choice. I also <a href="https://www.facebook.com/acgreenough" class="facebook" target="_blank">Facebook</a>, <a href="https://uk.linkedin.com/in/adamgreenough" class="linkedin" target="_blank">LinkedIn</a>, <a href="https://www.instagram.com/adam_greenough/" class="instagram" target="_blank">Instagram</a>, <a href="https://dribbble.com/xadz" class="dribbble" target="_blank">Dribbble</a> and email (but you have to ask for that one, or look hard enough).
+				</div>
+			</div>
+		</section>
+
+<?php theme_include('footer'); ?>
