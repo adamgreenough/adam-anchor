5d8e3d877dba685d62f78302689cd4ce378bea17
Adam Greenough <adam@amazorize.com>
2016-10-12 04:29:50 +0100

    Preloader fixes

--- footer.php
+++ footer.php
@@ -27,13 +27,11 @@
 		</script>
 		
 		<script type="text/javascript">
-			//<![CDATA[
 	        jQuery(window).on('load', function() { // makes sure the whole site is loaded 
 	            jQuery('#status').fadeOut(); // will first fade out the loading animation 
 	            jQuery('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
 	            jQuery('body').delay(350).css({'overflow':'visible'});
 	        })
-		    //]]>
 		</script>
     </body>
 </html>
